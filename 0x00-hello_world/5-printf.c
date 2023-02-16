f8f5659cd3eef7c51550dd271a6005ca2d8bbc0e
sabastian yalley <yalleysabastian@gmail.com>
2023-02-16 12:12:54 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio. h>
+/**
+ * main - A program that line with betty
+ * Return: 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
