5b967e5cd28148e6795e6f69c43f7249a0c4c165
sabastian yalley <yalleysabastian@gmail.com>
2023-02-16 14:45:33 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that involves betty
- * Return: 0
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
