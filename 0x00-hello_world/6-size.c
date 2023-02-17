c9fab22e4038b6680412be4715f32099c76103fa
sabastian yalley <yalleysabastian@gmail.com>
2023-02-16 16:45:16 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A programming that involves betty
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+int b;
+long nt c;
+long long int d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(b));
+print("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c
+));
+printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("size  of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+return (0);
