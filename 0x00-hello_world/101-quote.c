f39aa8e9e27f49b288109e43d676683bcda7a0af
sabastian yalley <yalleysabastian@gmail.com>
2023-02-16 18:53:23 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A programming that involves betty
+ * main - Entry point
  * Return 1 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+        return (1);
 }
